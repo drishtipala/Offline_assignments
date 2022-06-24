ef4a275f90a39e858da26268fb0abbfcf7282672
drishtipala <105123081+drishtipala@users.noreply.github.com>
2022-06-24 12:25:46 +0530

    Add files via upload

---
+++ Operations/Logical.java
@@ -0,0 +1,23 @@
+//WAP to accept number from the user and perform using logical operator
+
+package java_practicals;
+
+import java.util.Scanner;
+
+public class Logical {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num1,num2;
+		boolean res;
+		System.out.println("Enter two numbers:");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		res = num1>num2 && num1!=num2;
+        System.out.println("Result:"+res);
+        res = num1>num2 || num1!=num2;
+        System.out.println("Result:"+res);
+	}
+
+}
